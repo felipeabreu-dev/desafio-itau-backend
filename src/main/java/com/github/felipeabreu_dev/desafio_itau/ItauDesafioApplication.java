aa0332c631d784037f01634c25e4babad7425a88
Felipe Abreu <abreufelipe12@hotmail.com>
2025-11-08 12:33:34 -0300

    Criação do Projeto

---
+++ src/main/java/com/github/felipeabreu_dev/desafio_itau/ItauDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.github.felipeabreu_dev.desafio_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/felipeabreu_dev/desafio_itau/ItauDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.felipeabreu_dev.desafio_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
